c1ed8311698b0f081dd267d809338163f8e9b506
Michael Gibson <121519430+gibbato@users.noreply.github.com>
2024-07-01 12:18:40 -0700

    Add files via upload

--- socket_webserver.py
+++ socket_webserver.py
@@ -1,113 +1,71 @@
-try:
-  import usocket as socket
-except:
-  import socket
+import network
+import time
 import secrets
 from machine import Pin
-import network
+import socket
 
-import gc
-gc.collect()
+pin = Pin("LED", Pin.OUT)
 
 ssid = secrets.SSID
 password = secrets.PASSWORD
 
-station = network.WLAN(network.STA_IF)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
 
-station.active(True)
-station.connect(ssid, password)
+wlan.connect(ssid, password)
 
-while station.isconnected() == False:
-  pass
+# wait for connect or fail
+max_wait = 10
 
-print('Connection successful')
-status = station.ifconfig()
-print('ip = ' + status[0])
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print("waiting for connection...")
+    time.sleep(1)
+    
+if wlan.status() != 3:
+    raise RuntimeError('Network Connection Failed')
+else:
+    print('connected')
+    pin.value(1)
+    status = wlan.ifconfig()
+    print('ip = ' + status[0])
 
-led = Pin('LED', Pin.OUT)
-led_state = "OFF"
-
-def web_page():
-    html = """<html>
-
+html = """<!DOCTYPE html>
+<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <style>
-        html {
-            font-family: Arial;
-            display: inline-block;
-            margin: 0px auto;
-            text-align: center;
-        }
-
-        .button {
-            background-color: #ce1b0e;
-            border: none;
-            color: white;
-            padding: 16px 40px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-        }
-
-        .button1 {
-            background-color: #000000;
-        }
-    </style>
+    <title>ESP Web Server</title>
 </head>
-
 <body>
-    <h2>Raspberry Pi Pico Web Server</h2>
-    <p>LED state: <strong>""" + led_state + """</strong></p>
-    <p>
-        <i class="fas fa-lightbulb fa-3x" style="color:#c81919;"></i>
-        <a href=\"led_on\"><button class="button">LED ON</button></a>
-    </p>
-    <p>
-        <i class="far fa-lightbulb fa-3x" style="color:#000000;"></i>
-        <a href=\"led_off\"><button class="button button1">LED OFF</button></a>
-    </p>
+    <h1>ESP Web Server</h1>
+    <p>LED is now ON</p>
 </body>
+</html>
+"""
 
-</html>"""
-    return html
-
-
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('', 80))
-s.listen(5)
+s.bind(addr)
+s.listen(1)
 
+print('listening on', addr)
+# Listen for connections
 while True:
     try:
-        conn, addr = s.accept()
-        conn.settimeout(3.0)
-        print('Received HTTP GET connection request from %s' % str(addr))
-        request = conn.recv(1024)
-        conn.settimeout(None)
-        request = str(request)
-        print('GET Rquest Content = %s' % request)
-        led_on = request.find('/led_on')
-        led_off = request.find('/led_off')
-        if led_on == 6:
-            print('LED ON -> GPIO25')
-            led_state = "ON"
-            led.on()
-        if led_off == 6:
-            print('LED OFF -> GPIO25')
-            led_state = "OFF"
-            led.off()
-        response = web_page()
-        conn.send('HTTP/1.1 200 OK\n')
-        conn.send('Content-Type: text/html\n')
-        conn.send('Connection: close\n\n')
-        conn.sendall(response)
-        conn.close()
+        cl, addr = s.accept()
+        print('Client connected from:', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        response = html
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+        
     except OSError as e:
-        conn.close()
-        print('Connection closed')
+        cl.close()
+        print('connection closed')
